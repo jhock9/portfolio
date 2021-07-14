22e42e2847d069daa8d871360a1dc589cd9b1a06
jhock9 <jon.hocker@gmail.com>
2021-07-14 17:34:37 -0500

    updating contact form

--- php/contact-form.php
+++ php/contact-form.php
@@ -6,7 +6,7 @@ if (isset($_POST['Email'])) {
 
     function problem($error)
     {
-        echo "We're sorry, but there were error(s) found with the form you submitted. ";
+        echo "We're sorry, but there were error(s) found with the form you submitted.";
         echo "These errors appear below.<br><br>";
         echo $error . "<br><br>";
         echo "Please go back and fix these errors.<br><br>";
@@ -15,16 +15,18 @@ if (isset($_POST['Email'])) {
 
     // validation
     if (
-        !isset($_POST['Name']) ||
-        !isset($_POST['Email']) ||
-        !isset($_POST['Message'])
+        !isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['number']) ||
+        !isset($_POST['message'])
     ) {
         problem("We're sorry, but there appears to be a problem with the form you submitted.");
     }
 
-    $name = $_POST['Name']; // required
-    $email = $_POST['Email']; // required
-    $message = $_POST['Message']; // required
+    $name = $_POST['name']; // required
+    $email = $_POST['email']; // required
+    $number = $_POST['number']; // required
+    $message = $_POST['message']; // required
 
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
